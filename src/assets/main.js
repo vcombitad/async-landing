0cf51c5f288c0df4bf99de426c3d24ad70641385
vcombitad <vcombitad@ucentral.edu.co>
2023-03-22 12:58:32 -0500

    Ahora la landing tiene vinculo a youtube para reproducir el video, para q no sea sólo una imagen

--- src/assets/main.js
+++ src/assets/main.js
@@ -20,6 +20,7 @@ async function fetchData(urlApi) {
         const videos= await fetchData(API);
         let view= `
         ${videos.items.map(video=> `
+        <a href= "https://youtube.com/watch?v=${video.id.videoId}"target="_blank">
         <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
@@ -32,6 +33,7 @@ async function fetchData(urlApi) {
           </h3>
         </div>
       </div>
+      </a>
         `).slice(0,8).join('')}        
         `;
     content.innerHTML= view;
